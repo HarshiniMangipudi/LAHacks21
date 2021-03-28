39aca8c9d8fca79621e4bb0065dac7ab26a1e90e
Harshini Mangipudi <harshinimangipudi@gmail.com>
2021-03-27 18:28:07 -0700

    changed task fields

--- pages/models.py
+++ pages/models.py
@@ -28,6 +28,17 @@ class Task(models.Model):
     date_added = models.DateTimeField(default=timezone.now, verbose_name="Date Added")
     date_due = models.DateTimeField(default=(datetime.utcnow() + timedelta(days=2)), verbose_name="Date Due")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Sunday = models.BooleanField(default=True)
+    Monday = models.BooleanField(default=True)
+    Tuesday = models.BooleanField(default=True)
+    Wednesday = models.BooleanField(default=True)
+    Thursday = models.BooleanField(default=True)
+    Friday = models.BooleanField(default=True)
+    Saturday = models.BooleanField(default=True)
+    time_of_day = models.TimeField(default=timezone.now)
+    friend_fb_id = models.CharField(max_length=300, blank=False, default='enter user id')
+
+    
 
     def __str__(self):
         return self.task_name
